0520d840c3f1a60c26666e1e072085bd94dde1c4
Paulo Santiago <pauloecsantiago@gmail.com>
2024-03-19 11:06:21 +0000

    Mirage
    Setting up mirage

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Vans() {
+  let [vanInfo, setVanInfo] = useState("");
   return (
     <div>
       <h1>Vans page goes here 🚐</h1>
