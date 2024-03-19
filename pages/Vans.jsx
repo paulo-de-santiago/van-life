ecb79778021f4a21639064c6ef4d8658e7ab7ad7
Paulo Santiago <pauloecsantiago@gmail.com>
2024-03-19 11:19:13 +0000

    Van compoenent rendering
    vans component fethcing data rendering on page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -3,9 +3,26 @@ import { useState, useEffect } from "react";
 
 export default function Vans() {
   let [vanInfo, setVanInfo] = useState("");
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json)
+
+      .then((data) => setVanInfo(data.vans));
+  }, []);
+
   return (
     <div>
       <h1>Vans page goes here 🚐</h1>
+
+      {vanInfo &&
+        vanInfo.map((van) => (
+          <div>
+            <h1>{van.name}</h1>
+            <p>{van.price}</p>
+            <p>{van.description}</p>
+          </div>
+        ))}
     </div>
   );
 }
